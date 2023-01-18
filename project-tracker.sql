e2244fef39714fa577d7300587081ef3d8087a19
btarnow <beckytarnowski@gmail.com>
2023-01-18 15:53:19 -0500

    finished lab

--- project-tracker.sql
+++ project-tracker.sql
@@ -21,6 +21,42 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: beckytarnowski
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO beckytarnowski;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: beckytarnowski
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO beckytarnowski;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: beckytarnowski
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: beckytarnowski
 --
 
@@ -45,6 +81,59 @@ CREATE TABLE public.students (
 
 
 ALTER TABLE public.students OWNER TO beckytarnowski;
+
+--
+-- Name: report_card_all; Type: VIEW; Schema: public; Owner: beckytarnowski
+--
+
+CREATE VIEW public.report_card_all AS
+ SELECT students.first_name,
+    students.last_name,
+    projects.title,
+    projects.max_grade,
+    grades.grade
+   FROM ((public.students
+     LEFT JOIN public.grades ON (((students.github)::text = (grades.student_github)::text)))
+     LEFT JOIN public.projects ON (((projects.title)::text = (grades.project_title)::text)));
+
+
+ALTER TABLE public.report_card_all OWNER TO beckytarnowski;
+
+--
+-- Name: report_card_view; Type: VIEW; Schema: public; Owner: beckytarnowski
+--
+
+CREATE VIEW public.report_card_view AS
+ SELECT students.first_name,
+    students.last_name,
+    projects.title,
+    projects.max_grade,
+    grades.grade
+   FROM ((public.students
+     JOIN public.grades ON (((students.github)::text = (grades.student_github)::text)))
+     JOIN public.projects ON (((projects.title)::text = (grades.project_title)::text)));
+
+
+ALTER TABLE public.report_card_view OWNER TO beckytarnowski;
+
+--
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: beckytarnowski
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: beckytarnowski
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	sdevelops	Markov	50
+3	sdevelops	Blockly	100
+4	jhacks	Blockly	2
+\.
+
 
 --
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: beckytarnowski
@@ -73,6 +162,21 @@ btarnow	Becky	Tarnowski
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: beckytarnowski
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 5, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: beckytarnowski
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: beckytarnowski
 --
 
@@ -86,6 +190,22 @@ ALTER TABLE ONLY public.projects
 
 ALTER TABLE ONLY public.students
     ADD CONSTRAINT students_pkey PRIMARY KEY (github);
+
+
+--
+-- Name: grades grades_project_title_fkey; Type: FK CONSTRAINT; Schema: public; Owner: beckytarnowski
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_project_title_fkey FOREIGN KEY (project_title) REFERENCES public.projects(title);
+
+
+--
+-- Name: grades grades_student_github_fkey; Type: FK CONSTRAINT; Schema: public; Owner: beckytarnowski
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_student_github_fkey FOREIGN KEY (student_github) REFERENCES public.students(github);
 
 
 --
